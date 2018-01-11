b0169e5cb15493911ee592f76dc37aecedea7a9b
miguelf6022e23 <miguelf6.022e23@gmail.com>
2018-01-11 17:42:22 -0500

    db connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 8889,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+var connection;
+if (){
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+	connection = mysql.createConnection({
+	  host: "localhost",
+	  port: 8889,
+	  user: "root",
+	  password: "root",
+	  database: "burgers_db"
+	});
+} 
 
 
 connection.connect(function(err) {
